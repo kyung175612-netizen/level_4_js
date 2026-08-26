b99050e6f8c44c6ff65722f14e27dfe2be6a7282
kyung175612-netizen <kyung175612@gmail.com>
2026-08-26 12:50:58 +0900

    참, 거짓, 이벤트 함수

--- example-project-login-main/student/scripts/login.js
+++ example-project-login-main/student/scripts/login.js
@@ -32,19 +32,101 @@ function loginTabFunc(index){
 const userId = document.querySelector('#usdr-id');
 const memberLoginBtn = document.querySelector('#member-login');
 const userOrder = document.querySelector()
+
+//기존회원 - 아이디, 비밀번호, 로그인 버튼 변수
+const userIdInput = document.querySelector('#user-id');
+const userPw = document.querySelector('#user-pw');
+const memberLoginBtn = document.querySelector('#member-login');
+
+//비회원 - 주문자명, 주문번호, 비회원 주문비밀번호, 주문조회 버튼 변수
+const non_userId = document.querySelector('#non-user-id');
+const non_userOrder = document.querySelector('#non-user-order');
+const non_userPW = document.querySelector('#non-user-pw');
+
+const nonMemberLoginBtn = document.querySelector('#non-user-login');
+const pwVisibleBtn = document.querySelector('#non-member-pw-visible');
+const non_pwVisibleBtn = document.querySelector('#non-member-pw-visible');
+
 console.log(userId, memberLoginBtn);//변수 테스트
 
-non-memberLoginBtn.addEventListener('click',()=>{
-    loginErrFunc(non_userId, '주문자명');
-    loginErrFunc(non_userOrder, '주문번호');
-    loginErrFunc(non_userPw, '비회원 주문 비밀번호');
-})
+// non-memberLoginBtn.addEventListener('click',()=>{
+//     loginErrFunc(non_userId, '주문자명');
+//     loginErrFunc(non_userOrder, '주문번호');
+//     loginErrFunc(non_userPw, '비회원 주문 비밀번호');
+// })
 
-memberLoginBtn.addEventListener('click',()=>{ //기존 회원 로그인 버튼 이벤트
-    loginTabFunc(userId, '아이디'); //아이디 검사 함수 호출
-    loginErrFunc(userPw, '비밀번호'); //비밀번호 검사 함수 호출
-})
+// memberLoginBtn.addEventListener('click',()=>{ //기존 회원 로그인 버튼 이벤트
+//     loginTabFunc(userId, '아이디'); //아이디 검사 함수 호출
+//     loginErrFunc(userPw, '비밀번호'); //비밀번호 검사 함수 호출
+// })
 
 function loginErrFunc(dom, str){
     if(dom.value == ''){ alert(`$(str)를(을) 입력하세요`);}
 }
+
+//기존회원) 논리연산자 활용한 아이디&비밀번호 동시 검사 수행 이벤트 + 함수
+//목표1) 아이디, 비밀번호 중 하나라도 안적으면 'ooo을 입력하세요.'
+//목표2) 아이디, 비밀번호를 모두 적으면 'ooo님 환영합니다.'
+memberLoginBtn.addEventListener('click',()=>{
+    //목표1) 아이디, 비밀번호 중 하나라도 안적으면 'ooo을 입력하세요.'
+    // if(userId.value == '' && userPw.value == ''){}
+    if(userId.value == '' || userPw.value == ''){
+        if(userId.value == '' && userPw.value == ''){//조건세트의 시작
+            alert('아이디와 비밀번호를 입력하세요');
+        }
+        if(userId.value == ''){//하나의 조건세트에서 두번째 조건식이 필요할때(두개 이상 작성가능)
+            alert('아이디를 입력하세요');
+        }else{//조건세트 안에 거짓을 처리할때(마지막에 한번만 작성 가능)
+            alert('비밀번호를 입력하세요');
+        }
+    }
+    //목표2) 아이디, 비밃번호를 모두 적으면 'ooo님 환영합니다.'
+    /* if(userId.value != '' && userPw.value != ''){
+        alert('$(userId.value)님 환영합니다.');
+    } */
+    //상향조건 ? 참 : 거짓;
+    userId.value !=''&& userPw.value !=''?
+        alert('$(userId.value)님 환영합니다.'):null;
+})
+
+//비회원 이벤트 함수
+nonMemberLoginBtn.addEventListener('click',()=>{
+    //목표1) 주문자명, 주문번호, 비회원 주문 비번을 모두 썼을 때 'ooo님 주문은 배송중입니다.'
+    if(non_userId.value !='' && nonUserPw.value !=''){
+        alert('$(non_userId.value)님 주문은 배송중입니다.');
+    }
+    // if(non_userId.value !='' && nonUserPw.value !=''){
+    //     alert('$(non_userId.value)님 주문은 배송중입니다.');
+    // }
+
+    //목표2) 주문자명, 주문번호, 비회원 주문 비번 중 하나라도 안썼을 때 상황
+    if(non_userId.value == '' || nonUserPw.value == '' || nonUserPw.value == ''){
+        if(non_userId.value == '' && non_userOrder.value == ''&& nonUserPw.value == ''){
+            alert('주문자명, 주문번호, 주문 비밀번호를 입력해주세요.');
+        }else if(non_userId.value == ''){
+            alert('주문자명을 입력해주세요.');
+        }else if(non_userOrder.value == ''){
+            alert('주문번호를 입력해주세요.');
+        }else {
+            alert('주문비밀번호를 입력해주세요.');
+        }
+    }
+})
+
+//기존회원) 눈 아이콘 클릭 시 비밀번호 보이기/다시 누르면 비밀번호 숨기기
+let pwVisibleStatus = 0; //비번 숨김상태(****) 초기값
+pwVisibleBtn.addEventListener('click',()=>{
+    pwVisibleStatus == 0 ?
+        userPw.type='text' 
+        :   userPw.type = 'password'; 
+    pwVisibleStatus = !pwVisibleStatus;
+});
+
+//비회원 주문 비밀번호
+let non_pwVisiblesStatus = 0; //초기 숨김상태
+non_pwVisibleBtn.addEventListener('click',()=>{
+    non_pwVisiblesStatus == 0 ?
+        non_userPW.type = 'text'
+        : non_userPW.type = 'password';
+    non_pwVisiblesStatus = !non_pwVisiblesStatus;
+})
