3e49772a5dc6d89917f802e7156572e5fbf76119
kyung175612-netizen <kyung175612@gmail.com>
2026-09-04 11:33:15 +0900

    무비차트

--- movie_db_project_ex-main/scripts/index.js
+++ movie_db_project_ex-main/scripts/index.js
@@ -32,3 +32,47 @@ for(let i=0; i<9; i++){
     movieLi.innerHTML += `<a href="#"><img src="${animeDB[i].poster}" alt="${animeDB[i].title}"></a>`;
     animeWrap1.appendChild(movieLi);
 }
+
+//------------------------------------------------------------26/9/4
+//무비차트 + swiper
+// swiper-slide 반복 생성해서 swiper-wrapper 붙여넣기
+// swiper 플러그인 함수 연결은 swiper 최종 연결
+const chart_swiper = document.querySelector('.chart_swiper');
+
+const chart_swiper_func = new Swiper(chart_swiper, {//플러그인 연결
+    slidesPerView:2,
+    spaceBetween:10,
+}); 
+
+for(let i=0; i<5; i++){
+    const chart_slide = document.createElement('div'); //생성
+    chart_slide.classList.add('swiper-slide'); //클래스연결
+
+    chart_slide.innerHTML = `<p class="num">${moviesDB[i].id}</p>`;
+    chart_slide.innerHTML += `<h3">${moviesDB[i].title}</h3>`;
+    chart_slide.innerHTML += `<p class="rating">${moviesDB[i].rating}</p>`;
+    chart_slide.innerHTML += `<p class="story">${moviesDB[i].plot}</p>`;
+    chart_slide.style.backgroundImage = `url(${moviesDB[i].poster})`;
+
+    chart_swiper.children[0].appendChild(chart_slide); //붙여넣기
+}
+
+const anime_chart_swiper = document.querySelector('.anime_chart_swiper');
+
+const anime_chart_swiper_func = new Swiper(anime_chart_swiper, {//플러그인 연결
+    slidesPerView:2,
+    spaceBetween:10,
+}); 
+
+for(let i=0; i<5; i++){
+    const anime_chart_slide = document.createElement('div'); //생성
+    anime_chart_slide.classList.add('swiper-slide'); //클래스연결
+
+    anime_chart_slide.innerHTML = `<p class="num">${animeDB[i].id}</p>`;
+    anime_chart_slide.innerHTML += `<h3">${animeDB[i].title}</h3>`;
+    anime_chart_slide.innerHTML += `<p class="rating">${animeDB[i].rating}</p>`;
+    anime_chart_slide.innerHTML += `<p class="story">${animeDB[i].plot}</p>`;
+    anime_chart_slide.style.backgroundImage = `url(${animeDB[i].poster})`;
+
+    anime_chart_swiper.children[0].appendChild(anime_chart_slide); //붙여넣기
+}
